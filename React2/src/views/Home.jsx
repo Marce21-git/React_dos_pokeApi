dc80ebf4a6871efaf6283150b8e2b04b4808f721
Marcela Valenzuela Angotzi <130616628+Marce21-git@users.noreply.github.com>
2023-10-15 21:21:27 -0300

    Se cargan archivos al repositorio

---
+++ React2/src/views/Home.jsx
@@ -0,0 +1,15 @@
+const Home = () => {
+return(
+    <div className="mt -5 text-center">
+        <h1>Bienvenido maestro pokemón</h1>
+        <img 
+        height="300"
+        src="/assets/img/poke1.png"
+        alt="" 
+         />
+
+    </div>
+)
+
+}
+export default Home
